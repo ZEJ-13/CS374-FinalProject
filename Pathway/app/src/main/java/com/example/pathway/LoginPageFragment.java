3bbc9bfdf264dab6149b3497400284fa02d87358
ZEJ-13 <zej602@gmail.com>
2023-04-27 23:29:03 -0400

    fixed the error again now login works although create doesnt at least it wont crash

--- Pathway/app/src/main/java/com/example/pathway/LoginPageFragment.java
+++ Pathway/app/src/main/java/com/example/pathway/LoginPageFragment.java
@@ -1,31 +1,32 @@
 package com.example.pathway;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.pathway.databinding.LoginPageFragmentBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginPageFragment extends Fragment {
 
     private LoginPageFragmentBinding binding;
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = LoginPageFragmentBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
+    @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
@@ -36,6 +37,36 @@ public class LoginPageFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+
+        binding.buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.editTextEmail.getText().toString().trim();
+                String password = binding.editTextPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getContext(), "Please enter email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getContext(), "Please enter password", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                Toast.makeText(getContext(), "Login successful", Toast.LENGTH_LONG).show();
+                                NavHostFragment.findNavController(LoginPageFragment.this)
+                                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                            } else {
+                                Toast.makeText(getContext(), "Login failed", Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }
+        });
     }
 
     @Override
@@ -43,5 +74,4 @@ public class LoginPageFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
 }
